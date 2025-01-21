240301de3544a68fb273f2bf6426801574a16b5f
Mark Baggett <markpbaggett@gmail.com>
2025-01-21 16:49:29 -0600

    Describe an entire CSV.

--- htr_iiif_claude/htr.py
+++ htr_iiif_claude/htr.py
@@ -5,6 +5,7 @@ from htr_iiif_claude.manifest import Manifest
 import os
 import click
 from tqdm import tqdm
+from csv import DictReader
 
 
 @click.group()
@@ -59,6 +60,69 @@ def describe(image_uri: str, prompt: str, metadata: str, model: str, output: str
         with open(f"{output}.json", "w") as f:
             json.dump(y.text, f)
     print(f"Done. Total cost was: {y.cost.get('total')}. Results written to {output}")
+
+@cli.command("describe_csv", help="Describe images in a CSV")
+@click.option(
+    "--model",
+    "-m",
+    default="claude-3-haiku-20240307",
+    help="The model to use",
+)
+@click.option(
+    "--output",
+    "-o",
+    default="output",
+    help="The output directory to write text to",
+)
+@click.option(
+    "--csv",
+    "-c",
+    default="htr_describe.csv"
+)
+@click.option(
+    "--start",
+    "-s",
+    default="1",
+)
+@click.option(
+    "--end",
+    "-e",
+    default="10",
+)
+def describe_csv(model: str, output: str, csv: str, start: str, end: str) -> None:
+    total_images_to_get = int(end)
+    with open(csv, 'r') as csvfile:
+        i = 0
+        reader = DictReader(csvfile)
+        for _ in range(int(start) - 1):
+            next(reader)
+        for row in reader:
+            if i <= total_images_to_get:
+                fields_that_matter = (
+                "Item title", "Creator", "Publisher", "Category", "Location", "Collection", "Title",
+                "Creator (Photographer)", "Year (Coverage)", "Description", "Note", "Descriptive Notes", "Caption",
+                "Subject", "Event", "Keywords", "Drawing Name")
+                metadata_elements = []
+                for field in fields_that_matter:
+                    if row[field] != "":
+                        metadata_elements.append(f"{field}: {row[field]}")
+                image = Image(image_uri_or_path=row['Item link'])
+                y = ClaudeRequest(
+                    model=model,
+                    key=os.getenv("CLAUDE_API"),
+                    prompt=f'Give this image a title and a description based on its contents. Respond with the message '
+                           f'as output in JSON format with keys "title", "description" and values '
+                           f'as str. The image has the following metadata: {",".join(metadata_elements)}',
+                    image=image.hash,
+                )
+                if type(y.text) == str:
+                    with open(f"{output}/{row['Item link'].split('/')[-1].split('.')[0]}.txt", "w") as f:
+                        f.write(y.text)
+                elif type(y.text) == dict:
+                    with open(f"{output}/{row['Item link'].split('/')[-1].split('.')[0]}.json", "w") as f:
+                        json.dump(y.text, f)
+                print(f"Done. Total cost was: {y.cost.get('total')}. Results written to {output}/{row['Item link'].split('/')[-1].split('.')[0]}")
+                i += 1
 
 
 @cli.command("transcribe", help="Transcribe an image with Claude")
