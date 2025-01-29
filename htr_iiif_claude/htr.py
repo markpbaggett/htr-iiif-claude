ef8f89f73973d9ee502f34ccfa82a28f1879d15f
Mark Baggett <markpbaggett@gmail.com>
2025-01-29 13:57:43 -0600

    Add describe_csv and describe_simple.

--- htr_iiif_claude/htr.py
+++ htr_iiif_claude/htr.py
@@ -135,6 +135,87 @@ def describe_csv(model: str, output: str, csv: str, start: str, end: str) -> Non
                     print(f"Read timed out: {row['Item link']}\n")
                     i += 1
 
+@cli.command("describe_simple", help="Describe images in a CSV. Image must be a url in a CSV column called 'Image'")
+@click.option(
+    "--model",
+    "-m",
+    default="claude-3-haiku-20240307",
+    help="The model to use",
+)
+@click.option(
+    "--output",
+    "-o",
+    default="output",
+    help="The output directory to write text to",
+)
+@click.option(
+    "--csv",
+    "-c",
+    default="htr_describe.csv"
+)
+@click.option(
+    "--start",
+    "-s",
+    default="1",
+)
+@click.option(
+    "--end",
+    "-e",
+    default="10",
+)
+@click.option(
+    "--fields",
+    "-f",
+    default="title",
+    help="fields to use for metadata separated by |"
+)
+@click.option(
+    "--image_location",
+    "-i",
+    default="Medium Image",
+    help="The field with the image in the CSV",
+)
+def describe_simple(model: str, output: str, csv: str, start: str, end: str, fields:str, image_location: str) -> None:
+    fields_that_matter = fields.split('|')
+    total_images_to_get = int(end)
+    os.makedirs(output, exist_ok=True)
+    with open(csv, 'r') as csvfile:
+        i = 0
+        reader = DictReader(csvfile)
+        for _ in range(int(start) - 1):
+            next(reader)
+        for row in reader:
+            if i <= total_images_to_get:
+                try:
+                    metadata_elements = []
+                    for field in fields_that_matter:
+                        if row[field] != "":
+                            metadata_elements.append(f"{field}: {row[field]}")
+                    image = Image(image_uri_or_path=row[image_location])
+                    y = ClaudeRequest(
+                        model=model,
+                        key=os.getenv("CLAUDE_API"),
+                        prompt=f'Give this image a title and a description based on its contents. Respond with the message '
+                               f'as output in JSON format with keys "title", "description" and values '
+                               f'as str. The image has the following metadata: {",".join(metadata_elements)}',
+                        image=image.hash,
+                    )
+                    if type(y.text) == str:
+                        try:
+                            json_data = json.loads(y.text)
+                            with open(f"{output}/{row[image_location].split('/')[-1].split('.')[0]}.json", "w") as f:
+                                json.dump(json_data, f, indent=4)
+                        except json.decoder.JSONDecodeError:
+                            print(f"Couldn't convert string to json for {row[image_location]}\n")
+                    elif type(y.text) == dict:
+                        with open(f"{output}/{row[image_location].split('/')[-1].split('.')[0]}.json", "w") as f:
+                            json.dump(y.text, f, indent=4)
+                    print(f"Done. Total cost was: {y.cost.get('total')}. Results written to {output}/{row[image_location].split('/')[-1].split('.')[0]}")
+                    i += 1
+                except httpx.ReadTimeout as e:
+                    print(f"Read timed out: {row[image_location]}\n")
+                    i += 1
+
 
 @cli.command("transcribe", help="Transcribe an image with Claude")
 @click.option(
